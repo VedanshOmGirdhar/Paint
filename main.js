16756aef72c81cad8b354c7f2380c7156326dc04
VedanshOmGirdhar <80617090+VedanshOmGirdhar@users.noreply.github.com>
2022-04-05 18:50:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+var mouseEvent = "empty";
+var last_position_of_X, last_position_of_Y;
+color = "blue";
+width_of_line = 4;
+
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e){
+    mouseEvent = "mouseDown";
+}
+
+canvas.addEventListener("mouseup", my_mouseup);
+
+function my_mouseup(e){
+    mouseEvent = "mouseUP";
+}
+
+canvas.addEventListener("mouseleave", my_mouseleave);
+
+function my_mouseleave(e){
+    mouseEvent = "mouseleave";
+}
+
+canvas.addEventListener("mousemove", my_mousemove);
+
+function my_mousemove(e){
+current_position_of_mouse_X = e.clientX - canvas.offsetLeft;
+current_position_of_mouse_Y = e.clientY - canvas.offsetTop;
+
+if(mouseEvent == "mouseDown"){
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width_of_line;
+
+   ctx.arc(current_position_of_mouse_X, current_position_of_mouse_Y, 20, 0, 2*Math.PI);
+    ctx.stroke();
+}
+last_position_of_X = current_position_of_mouse_X;
+last_position_of_Y = current_position_of_mouse_Y;
+}
+
+
+
+
+
+
+
+
+
